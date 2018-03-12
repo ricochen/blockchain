56eee1ac7170a87f9ef83f591a9fa84e29c46e80
Rico Chen <ricochenx@gmail.com>
2018-03-11 23:43:21 -0700

    add address and transaction enum

---
+++ enums/address.js
@@ -0,0 +1,6 @@
+module.exports = {
+  bobAddress: '1EmhVvjm78fsHq1mGhxJbzR7EJdt4uRmXh',
+  aliceAddress: '1EmhVvjm78fsHq1mGhxJbzR7EJdt4uRmXh',
+  rewardAddress: '18uvwkMJsg9cxFEd1QDFgQpoeXWmmSnqSs',
+  feeAddress: '13h1DP2Boo9TAsenphroACxhNy7pGxDYXd'
+};
